a3680cd33a2c3f5eee2493e08221b058b950480e
Arturo Montoya <arturo@ascendify.com>
2017-10-28 03:58:53 -0700

    Demo DA0

---
+++ project1/src/chatApp/database/DemoDAO.java
@@ -0,0 +1,105 @@
+package chatApp.database;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+
+public class DemoDAO {
+
+    public static void main(String[] argv) {
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        HelloDAO  test = new HelloDAO();
+
+        try {
+            stmt = test.getConnection().createStatement();
+            rs = stmt.executeQuery("SELECT * FROM CUSTOMERS");
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            System.out.println("querying SELECT * FROM XXX");
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(columnValue + " " + rsmd.getColumnName(i));
+                }
+                System.out.println("");
+            }
+        }
+        catch (SQLException ex){
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        finally {
+            // it is a good idea to release
+            // resources in a finally{} block
+            // in reverse-order of their creation
+            // if they are no-longer needed
+
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                rs = null;
+            }
+
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException sqlEx) { } // ignore
+
+                stmt = null;
+            }
+        }
+
+    }
+}
+
+class HelloDAO {
+    Connection connection;
+    HelloDAO(){ this.connection = this.setConnection(); }
+    public Connection getConnection(){
+        return this.connection;
+    }
+    public Connection setConnection(){
+        System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            //return;
+        }
+
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+
+        try {
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/cmpe275?useLegacyDatetimeCode=false&serverTimezone=UTC","root", "pass");
+
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            //return;
+        }
+
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+        return connection;
+    }
+
+
+}
