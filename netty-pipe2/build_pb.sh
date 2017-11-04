3984d70ac2d9f5b166634501dbcf800f38d60957
Arturo Montoya <arturo@ascendify.com>
2017-11-04 01:04:12 -0700

    raft proto

--- netty-pipe2/build_pb.sh
+++ netty-pipe2/build_pb.sh
@@ -25,3 +25,6 @@ fi
 
 protoc --proto_path=${project_base}/resources --java_out=${project_base}/generated ${project_base}/resources/pipe.proto
 
+protoc --proto_path=${project_base}/resources --java_out=${project_base}/generated ${project_base}/resources/common.proto
+protoc --proto_path=${project_base}/resources --java_out=${project_base}/generated ${project_base}/resources/election.proto
+protoc --proto_path=${project_base}/resources --java_out=${project_base}/generated ${project_base}/resources/work.proto
