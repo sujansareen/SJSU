98e08ac19f440e5ccca0d2b7380113aec3e67e6f
Arturo Montoya <arturo@ascendify.com>
2017-11-17 19:07:20 -0800

    updated write client

--- Distributed-System-Project1/src/app/TestClient.java
+++ Distributed-System-Project1/src/app/TestClient.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,6 +9,7 @@ import java.io.FileInputStream;
 import com.google.protobuf.ByteString;
 
 import client.CommInit;
+import io.netty.channel.Channel;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelOption;
@@ -33,7 +36,7 @@ public class TestClient {
 	
 	static EventLoopGroup group;
 	static final int chunkSize = 1024; // MAX BLOB STORAGE = Math,pow(2,15) -1 = 65535 Bytes 
-	
+
 	
 	public static void init(String host_received, int port_received)
 	{
@@ -50,9 +53,36 @@ public class TestClient {
 
 
 			// Make the connection attempt.
-			 channel = b.connect(host, port).syncUninterruptibly();
+//			 channel = b.connect(host, port).syncUninterruptibly();
+			// Start the connection attempt.
+			Channel ch = b.connect(host, port).sync().channel();
+			//
+			ChannelFuture lastWriteFuture = null;
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			for (;;) {
+				String line = in.readLine();
+				if (line == null) {
+					break;
+				}
 
-			
+				// Sends the received line to the server.
+				Route message = writeMessage(line,"client1");
+				lastWriteFuture = ch.writeAndFlush(message);
+
+				// If user typed the 'bye' command, wait until the server closes
+				// the connection.
+				if ("bye".equals(line.toLowerCase())) {
+					ch.closeFuture().sync();
+					break;
+				}
+			}
+
+			// Wait until all messages are flushed before closing the channel.
+			if (lastWriteFuture != null) {
+				lastWriteFuture.sync();
+			}
+
+
 			// want to monitor the connection to the server s.t. if we loose the
 			// connection, we can try to re-establish it.
 			// ClientClosedListener ccl = new ClientClosedListener(this);
@@ -70,7 +100,7 @@ public class TestClient {
 	
 	/* TODO: Group	*/
 
-	public static void writeMessage(String message,String destination_id){
+	public static Route writeMessage(String message,String destination_id){
 		Message.Builder msg=Message.newBuilder();
 		msg.setType(Message.Type.SINGLE);
 		msg.setSenderId("dhanashree");
@@ -84,12 +114,27 @@ public class TestClient {
 		route.setPath(Route.Path.MESSAGE);
 		route.setMessage(msg);
 		Route routeMessage= route.build();
-		
-		channel.channel().writeAndFlush(routeMessage);
-		
-		if (channel.isDone() && channel.isSuccess()) {
-			System.out.println("Msg sent succesfully:");
-		}
+		return routeMessage;
+	}
+	public static Route getMessages(String message,String destination_id){
+		Message.Builder msg=Message.newBuilder();
+		msg.setType(Message.Type.SINGLE);
+		msg.setSenderId("dhanashree");
+		msg.setPayload(message);
+		msg.setReceiverId(destination_id);
+		msg.setTimestamp("10:01");
+		msg.setAction(Message.ActionType.POST);
+
+		Route.Builder route= Route.newBuilder();
+		route.setId(123);
+		route.setPath(Route.Path.MESSAGES_REQUEST);
+		route.setMessage(msg);
+		Route routeMessage= route.build();
+		return routeMessage;
+//		channel.channel().writeAndFlush(routeMessage);
+//		if (channel.isDone() && channel.isSuccess()) {
+//			System.out.println("Msg sent succesfully:");
+//		}
 	}
 	
 
@@ -100,12 +145,9 @@ public class TestClient {
 		
 		System.out.println("Sent the message");
 		TestClient.init(host, port);
-		File file = new File("runtime/log.txt");
-		TestClient.writeMessage("Hello Dhanashree","client1");
-	
-		while(true){
-			
-		}
+//		File file = new File("runtime/log.txt");
+		//TestClient.writeMessage("Hello Dhanashree","client1");
+		//TestClient.getMessages("Hello Dhanashree","client1");
 		// TODO Auto-generated method stub
 		
 	}
