f2af9991ee6e1433e3b6ee2b42a84523ff2685d1
Karthik S Nair <karthikreads@gmail.com>
2017-11-18 16:15:46 -0800

    new changes 18-nov-2017

--- Distributed-System-Project1/src/app/MyConstants.java
+++ Distributed-System-Project1/src/app/MyConstants.java
@@ -16,10 +16,12 @@ public class MyConstants {
 	public static final String GROUP_NAME="Group1";
 	public static final String NODE_NAME="kartk";
 	public static final String NODE_IP="10.250.236.67";
+	public static final String SERVER_NODE_IP="127.0.0.1";
 	public static final String NODE_PORT="4567";
 	public static final String SECRET="secret";
 	public static final String UDP_IP_BROADCAST = "255.255.255.255";
 	public static final Integer UDP_PORT = 8888;
+	public static final Integer TEST_PORT = 7777;
 	
 	public static void main(String args[]) throws Exception{
 		String ip = "";
