12efca771081649326edf4c3b92f13c81104a18b
Arturo Montoya <arturo.montoya332@gmail.com>
2018-03-10 15:22:40 -0800

    Service Page, routes defined

--- CMPE272/webApp/routes/web.php
+++ CMPE272/webApp/routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('landingPage');
+Route::group([], function () {
+
+    Route::get('/', function () {
+        return view('services');
+    });
+    Route::get('/about', function () {
+        return view('services');
+    });
+
+    Route::get('/services', function () {
+        return view('services');
+    });
+    Route::get('/news', function () {
+        return view('services');
+    });
+    Route::get('/contacts', function () {
+        return view('services');
+    });
 });
