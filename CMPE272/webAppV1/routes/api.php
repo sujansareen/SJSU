473b8ec1397d685273c4b7150370e97dddbda89c
Arturo Montoya <arturo.montoya332@gmail.com>
2018-03-11 14:56:15 -0700

    fix

--- CMPE272/webAppV1/routes/api.php
+++ CMPE272/webAppV1/routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'public',
+], function() {
+    /*
+     * Route to get
+     */
+    //Route::get('url', 'Controllers@method');
+
+});
