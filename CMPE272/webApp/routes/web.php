6936bb26db3558fd8f2621a730144f4e65b93717
Arturo Montoya <arturo.montoya332@gmail.com>
2018-03-11 17:24:03 -0700

    add welcome route

--- CMPE272/webApp/routes/web.php
+++ CMPE272/webApp/routes/web.php
@@ -8,9 +8,6 @@
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-//Route::get('/', function () {
-//    return view('welcome');
-//});
 */
 
 
@@ -22,7 +19,9 @@ $info = [
     ["title"=>"Share memories & connect with people", "msg"=>"share home movies and slides with family and friends via email, Web galleries, Facebook"],
     ["title"=>"Instant access anytime, anywhere", "msg"=>"you can view digitized memories on your PC, Mac, iPhone, iPad or Android mobile devices"]
 ];
-
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 
 Route::group([], function () use ($info){
     Route::get('/', function () {
