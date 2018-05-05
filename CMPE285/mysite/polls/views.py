7a328be6ca6febbb7f0344f12378173411e35385
Arturo Montoya <montoya332@live.com>
2018-05-05 13:59:00 -0700

    bootstrap4 views

--- CMPE285/mysite/polls/views.py
+++ CMPE285/mysite/polls/views.py
@@ -53,17 +53,7 @@ def index(request):
     else:
         latest_question_list = Question.objects.order_by('-pub_date')[:5]
         context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    return render(request, 'polls/stockCalc.html', context)
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def calculate(request):
-    return JsonResponse({'foo':'bar'})
+def boostrap(request):
+    return render(request, 'polls/index.html', {})
