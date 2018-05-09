9161ef4405564d5174f31a003d1d70e00a4ae647
Arturo Montoya <montoya332@live.com>
2018-05-08 21:26:55 -0700

    home page

--- CMPE272/webApp/resources/views/containers/home.blade.php
+++ CMPE272/webApp/resources/views/containers/home.blade.php
@@ -9,7 +9,7 @@
         </ol>
         <div class="carousel-inner">
           <div class="carousel-item">
-            <img class="first-slide" src="http://via.placeholder.com/1500x300" alt="First slide">
+            <img class="first-slide" src="http://via.placeholder.com/300x300" alt="First slide">
             <div class="container">
               <div class="carousel-caption text-left">
                 <h1>Example headline.</h1>
@@ -19,7 +19,7 @@
             </div>
           </div>
           <div class="carousel-item">
-            <img class="second-slide" src="http://via.placeholder.com/1500x300" alt="Second slide">
+            <img class="second-slide" src="http://via.placeholder.com/300x300" alt="Second slide">
             <div class="container">
               <div class="carousel-caption">
                 <h1>Another example headline.</h1>
@@ -29,7 +29,7 @@
             </div>
           </div>
           <div class="carousel-item active">
-            <img class="third-slide" src="http://via.placeholder.com/1500x300" alt="Third slide">
+            <img class="third-slide" src="http://via.placeholder.com/300x300" alt="Third slide">
             <div class="container">
               <div class="carousel-caption text-right">
                 <h1>One more for good measure.</h1>
@@ -59,19 +59,19 @@
         <!-- Three columns of text below the carousel -->
         <div class="row">
           <div class="col-lg-4">
-            <img class="rounded-circle" src="http://via.placeholder.com/1500x300" alt="Generic placeholder image" width="140" height="140">
+            <img class="rounded-circle" src="http://via.placeholder.com/300x300" alt="Generic placeholder image" width="140" height="140">
             <h2>Heading</h2>
             <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
             <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
-            <img class="rounded-circle" src="http://via.placeholder.com/1500x300" alt="Generic placeholder image" width="140" height="140">
+            <img class="rounded-circle" src="http://via.placeholder.com/300x300" alt="Generic placeholder image" width="140" height="140">
             <h2>Heading</h2>
             <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
             <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
-            <img class="rounded-circle" src="http://via.placeholder.com/1500x300" alt="Generic placeholder image" width="140" height="140">
+            <img class="rounded-circle" src="http://via.placeholder.com/300x300" alt="Generic placeholder image" width="140" height="140">
             <h2>Heading</h2>
             <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
             <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
